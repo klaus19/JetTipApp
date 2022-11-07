47fe9af25f33eaa6776df859ccc243f7e9479d13
klaus19 <tejaskhartude@gmail.com>
2022-11-07 13:41:36 +0530

    Initial commit

---
+++ app/src/main/java/com/example/jettipapp/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.jettipapp
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.jettipapp.ui.theme.JetTipAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+             MyApp {
+                 Text(text = "Hello Again")
+             }
+        }
+    }
+}
+@Composable
+fun MyApp(content: @Composable () -> Unit) {
+    JetTipAppTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(color = MaterialTheme.colors.background) {
+            content()
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+   MyApp {
+       Text(text = "Hello Again")
+   }
+}
